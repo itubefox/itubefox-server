cb0b5b5bae31d0210df9f624d1b0ea82a0ae3b18
iTubefox <63176754+itubefox@users.noreply.github.com>
2020-04-11 19:50:50 -0300

    Add files via upload

--- app/routes.js
+++ app/routes.js
@@ -5,18 +5,18 @@ const itubefox = require('./itubefox');
 function routes(host) {
 
     host.get('/', function (req, res) {
-        res.json({status: 200, message: 'Servidor iTubefox.', repo: 'https://github.com/itubefox/itubefox-server', license: 'https://github.com/itubefox/itubefox-server/blob/master/LICENSE'});
+        res.json({ status: 200, message: 'Servidor iTubefox.', repo: 'https://github.com/itubefox/itubefox-server', license: 'https://github.com/itubefox/itubefox-server/blob/master/LICENSE' });
     });
 
     host.get('/download', function (req, res) {
         res.set("Content-Type", "application/octet-stream");
-        res.set("Content-disposition", "attachment; filename=\""+decodeURIComponent(req.query.filename)+"\"");
+        res.set("Content-disposition", "attachment; filename=\"" + decodeURIComponent(req.query.filename) + "\"");
         request(req.query.url).pipe(res);
     });
 
     host.get('*', function (req, res) {
         res.status('404');
-        res.json({status: 404, message: 'Not found'});
+        res.json({ status: 404, message: 'Not found' });
     });
 
     host.post('/download/audio', function (req, res) {
@@ -32,15 +32,15 @@ function routes(host) {
 
                 if (is_cipher(data)) {
 
-                    obj_final.url = data.args.player_response.streamingData.adaptiveFormats[(data.args.player_response.streamingData.adaptiveFormats.length - 1)].cipher.url;
+                    obj_final.url = encodeURIComponent(data.args.player_response.streamingData.adaptiveFormats[(data.args.player_response.streamingData.adaptiveFormats.length - 1)].cipher.url);
                 } else {
 
-                    obj_final.url = data.args.player_response.streamingData.adaptiveFormats[(data.args.player_response.streamingData.adaptiveFormats.length - 1)].url;
+                    obj_final.url = encodeURIComponent(data.args.player_response.streamingData.adaptiveFormats[(data.args.player_response.streamingData.adaptiveFormats.length - 1)].url);
                 }
 
                 obj_final.details = data.args.player_response.videoDetails;
                 obj_final.thumbnail = data.args.player_response.videoDetails.thumbnail.thumbnails[(data.args.player_response.videoDetails.thumbnail.thumbnails.length - 1)].url;
-                obj_final.filename = data.args.player_response.videoDetails.title + '.mp3';
+                obj_final.filename = encodeURIComponent(filename_normalize('iTubefox ' + data.args.player_response.videoDetails.title) + '.mp3');
 
                 res.json(obj_final);
             });
@@ -62,15 +62,15 @@ function routes(host) {
 
                 if (is_cipher(data)) {
 
-                    obj_final.url = data.args.player_response.streamingData.formats[(data.args.player_response.streamingData.formats.length - 1)].cipher.url;
+                    obj_final.url = encodeURIComponent(data.args.player_response.streamingData.formats[(data.args.player_response.streamingData.formats.length - 1)].cipher.url);
                 } else {
 
-                    obj_final.url = data.args.player_response.streamingData.formats[(data.args.player_response.streamingData.formats.length - 1)].url;
+                    obj_final.url = encodeURIComponent(data.args.player_response.streamingData.formats[(data.args.player_response.streamingData.formats.length - 1)].url);
                 }
 
                 obj_final.details = data.args.player_response.videoDetails;
                 obj_final.thumbnail = data.args.player_response.videoDetails.thumbnail.thumbnails[(data.args.player_response.videoDetails.thumbnail.thumbnails.length - 1)].url;
-                obj_final.filename = data.args.player_response.videoDetails.title + '.mp4';
+                obj_final.filename = encodeURIComponent(filename_normalize('iTudefox ' + data.args.player_response.videoDetails.title) + '.mp4');
 
                 res.json(obj_final);
             });
@@ -78,6 +78,13 @@ function routes(host) {
             res.json({});
         });
     });
+}
+
+function filename_normalize(filename) {
+    return filename.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+        .replace(/([^\w]+|\s+)/g, '-')
+        .replace(/\-\-+/g, '-')
+        .replace(/(^-+|-+$)/, '');
 }
 
 function is_cipher(data) {
